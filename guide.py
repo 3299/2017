4827c187df656f33bf118e2113cfbfeffe24b20d
Max Isom <codetheweb@icloud.com>
2017-02-03 21:08:46 -0600

    Misc work on vision

--- guide.py
+++ guide.py
@@ -11,32 +11,54 @@ class Guiding(object):
         self.sonic = sonic
         self.chassis = chassis
 
+    def guideSonic(self):
+        difference = self.sonic.getCm('left') - self.sonic.getCm('right')
+        average = (self.sonic.getCm('left') + self.sonic.getCm('right')) / 2
+
+        difference = helpers.remap(difference, -40, 40, -1, 1)
+        if (difference < -1):
+            difference = -1
+        elif (difference > 1):
+            difference = 1
+
+        powerX = 20 - average
+        powerX = helpers.remap(powerX, -20, 60, -1, 1)
+
+        print('PowerX: ' + str(powerX) + ', rotation: ' + str(difference))
+        #self.chassis.cartesian(powerX, 0, difference)
+
+        return
+
     def guideCamera(self, power):
         try:
+            # contours
             centerX = self.sd.getNumberArray('x')
             centerY = self.sd.getNumberArray('y')
             areas   = self.sd.getNumberArray('areas')
 
-            if (len(centerX) != 2 or len(centerY) != 2): # if only one strip can be seen don't do anything
-                print('Cannot see two strips')
-                return
-
+            # camera viewport
             width = self.sd.getNumber('width')
             height = self.sd.getNumber('height')
 
-            strafe = (((width - centerX[0]) - (width - centerX[1])) / 2 + centerX[0]) * 0.02
+            if (len(centerX) != 2 or len(centerY) != 2): # if only one strip can be seen (or more than two strips) don't do anything
+                print('Cannot see two strips')
+                return
+
+            strafe = (((width - centerX[0]) - (width - centerX[1])) / 2 + centerX[0]) * 0.02 # center point of two targets relative to camera viewport, then scale down
             strafe = math.degrees(math.atan2(1, -strafe)) - 90
             power = ((areas[0] + areas[1]) / 2) * 0.0014
 
-            rotate = (areas[1] - areas[0])
+            # corrects so that robot is parallel with davit face
+            rotate = self.sonic.getCm('right') - self.sonic.getCm('left')
+            rotate = helpers.remap(rotate, -60, 60, 0, 360)
 
-            rotate = helpers.remap(rotate, -(width * height) / 10, (width * height) / 10, -1, 1)
+            #self.chassis.cartesian(power, strafe, rotate)
+            #self.chassis.cartesian(0.5, 0, 0)
 
-
-            #self.chassis.set(power, strafe, rotate)
             print('Strafe: ' + str(strafe))
             print('Rotation: ' + str(rotate))
             print('Power: ' + str(power))
 
         except:
+            print('error.vision: is the vision processing program running?')
             return
