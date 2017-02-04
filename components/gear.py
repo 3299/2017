ef168acc50dbacba540bc60207cab624eb9abdae
Max Isom <codetheweb@icloud.com>
2017-02-03 21:05:16 -0600

    Fix gear component (use builtin function)

--- components/gear.py
+++ components/gear.py
@@ -5,27 +5,9 @@ Pops the gear onto the peg. Uses solenoids.
 class GearSol(object):
     def __init__(self, sol):
         self.sol = sol
-        self.state = 'DOWN'
 
-    def run(self, trigger1, trigger2):
-        '''
-        if (trigger == True and self.state == 'UP'):
-            self.sol['in'].set(True)
-            self.sol['out'].set(False)
-            self.state = 'DOWN'
-
-        elif (trigger == True and self.state == 'DOWN'):
-            self.sol['out'].set(True)
-            self.sol['in'].set(False)
-            self.state = 'UP'
-        '''
-
-        if (trigger1 == True):
-            self.sol['in'].set(True)
+    def run(self, trigger):
+        if (trigger == True):
+            self.sol.set(self.sol.Value.kForward)
         else:
-            self.sol['in'].set(False)
-
-        if (trigger2 == True):
-            self.sol['out'].set(True)
-        else:
-            self.sol['out'].set(False)
+            self.sol.set(self.sol.Value.kReverse)
