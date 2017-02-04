a255c7f8c2817407419e35d96130604160c2d501
Max Isom <codetheweb@icloud.com>
2017-02-03 21:05:58 -0600

    Correct mapping

--- map.py
+++ map.py
@@ -10,19 +10,19 @@ class Map(object):
         self.rightJ      = 2
 
         # Motors have suffix 'M'. All motors use PWM.
-        self.frontLeftM  = 2
+        self.frontLeftM  = 0
         self.frontRightM = 1
-        self.backLeftM   = 0
+        self.backLeftM   = 2
         self.backRightM  = 3
 
-        self.beltM       = 9
+        self.collectorM  = 9
 
         # Soleniods
-        self.gearSol     = {'in': 0, 'out': 2}
+        self.gearSol     = {'in': 2, 'out': 0}
 
         # Sensors have suffix 'S'. Gyro and sonar use analog in, everything else uses the DIO.
         self.gyroS       = 0
-        self.sonicS      = {'left': 1, 'right': 2}
+        self.sonicS      = {'left': 1, 'right': 2} # pin 4 on both sensors is also connected to the same DIO pin
 
         # LED strip
         self.ledStrip    = {'r': 4, 'g': 5, 'b': 6}
