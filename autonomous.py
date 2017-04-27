7c4fc6d5a1bbfa830f7eb2e24334b9a55533e83d
Max Isom <codetheweb@icloud.com>
2017-04-27 16:03:54 -0500

    Center auto working

--- autonomous.py
+++ autonomous.py
@@ -20,31 +20,48 @@ class Autonomous(object):
         if (self.sd.getBoolean('autoAngle') == True):
             # Drive forward
             startTime = time.clock()
-            while (time.clock() - startTime < 3):
-                self.drive.driveToAngle(0.5, 0, True)
+            while (time.clock() - startTime < 2):
+                self.drive.driveToAngle(-0.5, 0, True)
 
             # Stop
             self.drive.cartesian(0, 0, 0)
 
             # Turn 60 or -60 degrees
             if (self.sd.getBoolean('isLeft') == True):
-                self.drive.driveToAngle(0, 60, False)
-            else:
                 self.drive.driveToAngle(0, -60, False)
+            else:
+                self.drive.driveToAngle(0, 60, False)
 
-        # Do vision
-        self.vision.alignToPeg(direction=1)
+            # Do vision
+            if (self.vision.alignToPeg(direction=1) == False): # if returns an error, stop auto
+                return False
 
-        # Activate back gear
-        self.backGear.run(True, 'out')
+            # Stop
+            self.drive.cartesian(0, 0, 0)
 
+        '''
+        Center peg
+        '''
+        startTime = time.clock()
+        while (time.clock() - startTime < 4.25):
+            self.drive.cartesian(0, -0.3, 0.025)
+        self.drive.cartesian(0, 0, 0)
+
+        # Activate front gear
+        self.frontGear.run(True)
+
+        '''
+        wpilib.Timer.delay(3)
+
+        self.backGear.run(False, False)
         wpilib.Timer.delay(2)
 
         # Drive away
         startTime = time.clock()
-        while (time.clock() - startTime < 3):
-            self.drive.cartesian(0, 0.2, 0)
+        while (time.clock() - startTime < 2):
+            self.drive.cartesian(0, -0.4, 0)
         self.drive.cartesian(0, 0, 0)
+        '''
 
         # Stop Randy
         self.randy.run(False)
